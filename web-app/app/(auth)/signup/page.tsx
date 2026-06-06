b221d8e977c8501eac27436964148c47afbd1c39
jagdep-singh <jagdeep.singh12a@gmail.com>
2026-06-06 16:22:24 +0530

    ground work for signup and login

--- web-app/app/(auth)/signup/page.tsx
+++ web-app/app/(auth)/signup/page.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+"use client"
 
-const signup = () => {
+import React from 'react'
+import { useState} from 'react'
+import { useAuthStore } from "@/store/auth-store"
+
+
+const Register = () => {
+  const signup = useAuthStore((state) => state.signup)
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  async function handleSubmit(e : React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    await signup(name, email, password)
+    console.log("registered")
+  }
+
   return (
-    <div>signup</div>
+    <main>
+        <form onSubmit= {handleSubmit} >
+                <label htmlFor="name">Name</label>
+                <input
+                    type="text"
+                    id="name"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
+            
+            
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+            
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            
+            <button type="submit">Sign Up</button>
+        </form>
+    </main>
   )
 }
 
-export default signup
+export default Register
